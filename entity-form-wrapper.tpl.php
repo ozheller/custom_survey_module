5644625ce3d433c419555e2fdb63c10decb33995
Ozymandius Heller <Oz@Ironman.local>
2016-12-26 10:26:59 -0600

    Custom Survey Module, includes survey logic, mail handler, and minor site changes.

---
+++ entity-form-wrapper.tpl.php
@@ -0,0 +1,6 @@
+<div class="<?php print $classes; ?> container"<?php print $attributes; ?>>
+  <?php print render($title_prefix); ?>
+  <?php print render($title_suffix); ?>
+
+  <?php print render($form); ?>
+</div>
